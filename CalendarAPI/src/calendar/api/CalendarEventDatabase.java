2f0c75bfdbf850d8f87d9dfd37667232cad3962e
wbigger <wbigger@gmail.com>
2018-07-18 16:40:17 +0200

    Add comment to still better clarify the goal of populate function :)

--- CalendarAPI/src/calendar/api/CalendarEventDatabase.java
+++ CalendarAPI/src/calendar/api/CalendarEventDatabase.java
@@ -12,6 +12,11 @@ import java.util.ArrayList;
  * @author claudio
  */
 public interface CalendarEventDatabase {
-    // Utility method to quickly populate an array of events
-    public void populate(ArrayList<CalendarEvent> events);
+    /**
+     * Utility method to quickly populate an array of events. 
+     * This method is needed to help the calendar manager to create an array of 
+     * events of a specific subclass of CalendarEvent.
+     * @param arrayToPopulate the array to be populated
+     */
+    public void populate(ArrayList<CalendarEvent> arrayToPopulate);
 }
