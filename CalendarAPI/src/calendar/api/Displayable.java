d374a356fcb1c9f94dacb29b168db2e431cf89c8
wbigger <wbigger@gmail.com>
2018-07-19 13:49:43 +0200

    Add interface to display objects

---
+++ CalendarAPI/src/calendar/api/Displayable.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calendar.api;
+
+/**
+ * Makes an object displayable by Calendar.
+ * @author claudio
+ */
+public interface Displayable {
+    public String getTitle();
+    public String getDescription();
+}
